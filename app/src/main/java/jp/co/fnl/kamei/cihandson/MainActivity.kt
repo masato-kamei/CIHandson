a13fc7892441f242d3122159485dfae28c5e2bcb
masato.kamei <masato.kamei@fujimic.com>
2026-06-25 15:03:42 +0900

    コンパイルエラーのコードを敢えてコミット

--- app/src/main/java/jp/co/fnl/kamei/cihandson/MainActivity.kt
+++ app/src/main/java/jp/co/fnl/kamei/cihandson/MainActivity.kt
@@ -35,7 +35,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Good afternoon $name!",
         modifier = modifier
-    )
+    )a
 }
 
 @Preview(showBackground = true)
